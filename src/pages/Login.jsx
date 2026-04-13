d2e5af9590958c404ff2d5b39e2fa3d83bbfae44
Paul Wamaria <paulwamaria@gmail.com>
2026-04-13 15:41:39 +0100

    Fix importation typo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { useAuth } from "../auth/authContext";
+import { useAuth } from "../auth/AuthContext";
 
 import { Card, CardHeader, CardBody, Input, Button } from "../components/UI";
 
